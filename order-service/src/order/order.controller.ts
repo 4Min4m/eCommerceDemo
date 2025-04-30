3918939c8eddf65c640b20b720cd4ad4645f1f40
Amin <36745993+4Min4m@users.noreply.github.com>
2025-04-30 18:34:05 +0000

    last commit

--- order-service/src/order/order.controller.ts
+++ order-service/src/order/order.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { Order } from './order.entity';
 
 @Controller('orders')
 export class OrderController {
   constructor(private orderService: OrderService) {}
+
   @Post()
   async create(@Body() body: { userId: string; amount: number }): Promise<Order> {
     return this.orderService.createOrder(body.userId, body.amount);
+  }
+
+  @Get()
+  async getStatus(): Promise<string> {
+    return 'Order Service is running!';
   }
 }
